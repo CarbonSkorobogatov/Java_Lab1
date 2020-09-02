6c56ab1ea501ffa233e408ac8dcd4be3a4d8abd4
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-09-02 16:36:13 +0300

    add

---
+++ src/com/company/Medicine.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Medicine {
+    String Name, Director, Actor, Date;
+    int views;
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    Level level;
+
+    public Medicine(String Name, String Director, String Actor, String Date, int Raiting, int views){
+        this.Name = Name;
+        this.Director = Director;
+        this.Actor = Actor;
+        this.Date = Date;
+        this.views = views;
+
+        if (Raiting < 7){
+            this.level = Level.HIGH;
+        }else if(Raiting < 4){
+            this.level = Level.MEDIUM;
+        }else{
+            this.level = Level.LOW;
+        }
+    }
+}
